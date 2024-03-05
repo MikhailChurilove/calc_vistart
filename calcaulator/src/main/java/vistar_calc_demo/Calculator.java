8d261302e525220d72ab9e4062af8a6cec77295b
Michail <mike.curilov@gmail.com>
2024-03-05 10:57:04 +0300

    Initial commit

---
+++ calcaulator/src/main/java/vistar_calc_demo/Calculator.java
@@ -0,0 +1,8 @@
+package vistar_calc_demo;
+
+public class Calculator
+{
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ main/src/main/java/vistar_calc_demo/App.java
@@ -0,0 +1,9 @@
+package vistar_calc_demo;
+
+public class App 
+{
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(1, 2));
+    }
+}
